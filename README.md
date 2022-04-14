32a1d2437488356fce07c0cab3cdc2c3f9b36ca9
Bryan Choong <bryanchoong12@gmail.com>
2022-04-14 10:15:31 +0800

    Init

---
+++ README.md
@@ -0,0 +1,55 @@
+# autoAttendix2
+The autoAttendix2 is a better version of [autoAttendix](https://github.com/bryanc12/autoAttendix). Which use the api instead of the web interface, which boost the performance significantly by skipping the process of waiting the web interface to load which takes a long time.
+
+You may use the included QR Code image files for testing and checking if everything is working as expected or correctly.
+
+## Requirements
+1. Python 3.10
+2. Internet :smiley:
+   
+## How to use
+1. Add your ApSpace login credentials and mode to settings.json file. <br> For Example:
+```json
+{
+    "username":"tp012345",
+    "password":"password",
+    "discord-bot":false,
+    "discord-webhook":"",
+    "power-saving":true
+}
+```
+ - Turning ```power-saving``` on(true) will capture that screen less frequently for resources saving purpose, by turning it off(false), the programm will take more resource and power consumption, but this increase the sensitivity of QR code recognition.
+ - Turning ```discord-bot``` on(true), the program will send message to the discord server whenever it log to console. By turning this on(true), the ```discord-webhook``` have to be a valid webhook address, more information about discord webhook can be found [below](#discord-webhook).
+  
+2. Click on start.bat and it will do their works.
+
+## Discord Webhook
+By using the discord webhook, the program will send you a message when a attendance signing process is attempted. This feature can be implemented to any private/public server. The webhook can generated with the steps below.
+
+1. Go to discord app homepage
+2. On the side navigation bar, scroll to the bottom
+3. Select the plus/add symbol button
+4. Select ```Create My Own```
+5. Select ```For me and my friends```
+6. Fill in your desire server name and click create
+7. Right click on the server created
+8. Select ```Server Settings```
+9. Select ```Integrations``` on the side navigation bar
+10. Select ```View Webhooks```
+11. Select ```New Webhook```
+12. Fill in the Webhook ```Name``` and choose your desire ```Channel```(#general as default)
+13. Select ```Copy Webhook URL```
+14. Paste the URL into the ```settings.json``` as example below, and set the ```discord-bot``` to ```true```
+```json
+{
+    "discord-bot":true,
+    "discord-webhook":"https://discord.com/api/webhooks/963589414606789804/RtZ_PnmMV_Ywxu132uTvSQn_xCjKdddawawdjw-GxY47AaUNe2kdwDWfSENyplS",
+}
+```
+15. Done
+
+## To do:
+ - Multi User :hourglass:
+ - Telegram Integration :hourglass:
+ - Instagram Integration :running_man:
+ - Discord Integration :heavy_check_mark:
---
+++ main.py
@@ -0,0 +1,171 @@
+import gc
+import json
+import time
+from datetime import datetime
+
+import cv2
+import numpy as np
+import pyzbar.pyzbar as pyzbar
+import requests
+from PIL import ImageGrab
+from pyzbar.pyzbar import ZBarSymbol
+
+
+def main():
+    global DiscordWebhook, DiscordEnabled
+    username, password, powerSaving, DiscordEnabled, DiscordWebhook = getSettings()
+    print('Start Tracking Attendance...\n')
+
+    # Define variables for checking use
+    lastOtpCode = None
+    lastSignTime = time.time()
+
+    # Loop for OTP Code and Sign after settings readed from settings file
+    while True:
+        while True:
+            otpCode = getOtpCode(powerSaving)
+
+            # If OTP Code is None type(Never attempt to sign otp before) or the OTP Code is 
+            # different from from the last signed code then attempt to sign the OTP Code 
+            if (otpCode == None) or (otpCode != lastOtpCode):
+                break
+
+            # When the OTP Code is same as the last signed OTP Code, check if it is 5mins ago
+            # If yes, attempt to sign the OTP Code again, else sleep for 5 seconds
+            lastSignGap = time.time() - lastSignTime
+            if (lastSignGap > 300.0):
+                break
+            else:
+                pass
+                time.sleep(5)
+
+        log('OTP Code Found -> ' + otpCode)
+        signAttendance(otpCode, username, password)
+        lastOtpCode = otpCode
+        lastSignTime = time.time()
+
+        gc.collect()
+
+
+def getSettings():
+    # read credentials from settings.json
+    data = json.loads(open('.\settings.json', 'r').read())
+    # Check if credentials are not empty
+    if not (bool(data['username']) or bool(data['password'])):
+        print('Please fill in your username & password!')
+        exit()
+    # Check if mode are valid(if mode are not True or False)
+    if (type(data['power-saving']) != bool):
+        print('Please fill in the power mode(true/false)!')
+        exit()
+    # Check if discord-bot is enabled
+    if ((data['discord-bot'] == True) and not bool(data['discord-webhook'])):
+        print('Please fill in the discord webhook, check documentation for more info!') 
+        exit()
+    
+    return (data['username'], data['password'], data['power-saving'], data['discord-bot'], data['discord-webhook'])
+
+
+def getOtpCode(powerSaving):
+    # Loop until an OTP Code QR code is found
+    while True:
+        # Try to capture the screen
+        try:
+            img = ImageGrab.grab()
+        except OSError:
+            # If OS deny capture of screen, handle the exeption and screen capture again 
+            print('Screen capture permission denied by Operating System, error handled and solved.\n')
+        # Turn pixels data into array form
+        imgNp = np.array(img)
+        # Turn pixels array from color to greyscale
+        frame = cv2.cvtColor(imgNp, cv2.COLOR_BGR2GRAY)
+        # Scan picture for capturing QR code
+        decodedData = pyzbar.decode(frame, symbols=[ZBarSymbol.QRCODE])
+        sorted(ZBarSymbol.__members__.keys())
+        # Convert scanned object object into string, and check it is a valid QR code in 3 digit form
+        for obj in decodedData:
+            otpCode = obj.data.decode('utf-8')
+            if otpCode.isdecimal():
+                if (len(otpCode) > 0) and (len(otpCode) < 4):
+                    return otpCode
+        gc.collect()
+
+        if powerSaving:
+            time.sleep(1)
+        else:
+            time.sleep(0.1)
+
+
+def signAttendance(otpCode, username, password):
+
+    refreshTokenRequestUrl = 'https://cas.apiit.edu.my/cas/v1/tickets'
+    signApiUrl = 'https://attendix.apu.edu.my/graphql'
+
+    # Common header for questing refresh token and api request ticket
+    commonHeader = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'text/plain',
+    }
+
+    refreshTokenRequestParameter = {
+        'username': username,
+        'password': password
+    }
+
+    refreshTokenResponse = requests.post(url=refreshTokenRequestUrl, headers=commonHeader, params=refreshTokenRequestParameter)
+    refreshToken = refreshTokenResponse.text
+
+    ticketRequestUrl = 'https://cas.apiit.edu.my/cas/v1/tickets/' + refreshToken + '?service=https://api.apiit.edu.my/attendix'
+    ticketRequestParameter = {
+        'service':'https://api.appit.edu.my/attendix'
+    }
+
+    ticketRequestResponse = requests.post(url=ticketRequestUrl, headers=commonHeader, params=ticketRequestParameter)
+    ticket = ticketRequestResponse.text
+
+    signHeader = {
+        'accept':'application/json',
+        'content-type':'application/json',
+        'ticket':ticket,
+        'x-amz-user-agent':'aws-amplify/2.0.7',
+        'x-api-key':'da2-dv5bqitepbd2pmbmwt7keykfg4'
+    }
+
+    signVariables = {
+        'otp':otpCode
+    }
+
+    signQuery = 'mutation updateAttendance($otp: String!) {\n  updateAttendance(otp: $otp) {\n    id\n    attendance\n    classcode\n    date\n    startTime\n    endTime\n    classType\n    __typename\n  }\n}\n'
+
+    signJson = {
+        'operationName':'updateAttendance',
+        'variables':signVariables,
+        'query':signQuery
+    }
+
+    signResponse = requests.post(url=signApiUrl, headers=signHeader, json=signJson)
+    signResponseJson = signResponse.json()
+
+    if signResponseJson['data'] is not None:
+        log('Attendance successfully taken for ' + signResponseJson['data']['updateAttendance']['classcode'] + '\n')
+    else:
+        log(signResponse.json()['errors'][0]['message'] + '\n')
+
+
+def log(rawMsg):
+    # Get current date time in defined style
+    dateTime = datetime.now().strftime('%m/%d/%Y, %H:%M:%S')
+    msg = (dateTime + ' ' + rawMsg)
+    print(msg)
+
+    # Send message to discord server if webhook provided
+    if DiscordEnabled:
+        discordJson = {'content':msg}
+        requests.post(url=DiscordWebhook, json=discordJson)
+
+    # Append msg to latest.log
+    with open('latest.log', 'a') as logFile:
+        logFile.write(msg +'\n')
+
+if __name__ == '__main__':
+    main()
